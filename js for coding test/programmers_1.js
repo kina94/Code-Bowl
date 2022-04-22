cc86a790679fd94ecea21303e21ba99eb365f66d
열일 <isawthat77@naver.com>
2022-04-22 18:58:58 +0900

    20220422

--- js for coding test/programmers_1.js
+++ js for coding test/programmers_1.js
@@ -1,7 +1,5 @@
 function solution(id_list, report, k) {
     report_result = {} // 피신고인과 신고인 매칭 객체
-    report_array = {} //3번 이상 동일인에게 신고당한 경우를 제외한 객체
-    report_cnt = {} //신고당한 횟수
     answer={} // 정지 메일 발송 횟수
 
     id_list.map(name=>{
@@ -9,29 +7,22 @@ function solution(id_list, report, k) {
         answer[name]=0
     }) // 신고인과 피신고인, 정지 메일 발송 횟수 초기화
 
-    report.map(key=>{
-        report_array[key] = (report_array[key] || 0)+1
-    }) // 3번 이상 동일인에게 신고당한 경우를 제외하기 위해 report 배열의 중복 횟수 확인
 
-    report_array = Object.keys(report_array).filter(key=>report_array[key]<3) // 중복된 횟수 (3번 이상 동일인에게 신고당한 경우)를 제외한 배열 반환
-
-    report_array.map(name=>{
+    report.map(name=>{
         const [report, reported] = name.split(' ')
-        report_cnt[reported] = (report_cnt[reported] || 0 )+1
-        report_result[report].push(reported)
-    }) // 피신고인이 신고당한 횟수 및 신고인과 피신고인 매칭 객체 생성
+        if(!report_result[reported].includes(report)){
+            report_result[reported].push(report)
+        }
+    }) // 피신고인 : [신고인 배열] 객체 생성
 
-    report_cnt = Object.keys(report_cnt).filter(key=>report_cnt[key]>=k) // k번 이상 신고당한 피신고인만 포함하는 배열 추출
-
-    Object.keys(report_result).map(reporter=>{
-        report_cnt.map(reported=>{
-            if(report_result[reporter].includes(reported)){
-                answer[reporter] = ( answer[reporter] || 0 ) +1
-            }
-        })
-    }) // k번 이상 신고당한 피신고인 값을 가지고 있는 신고인 key값을 cnt++
-    // 초기에 answer 배열을 0으로 초기화하고, 중복 신고의 경우 제외했기 때문에 1번의 경우를 제외한 나머지는 0으로 카운트됨
-
+    Object.keys(report_result).map(name=>{
+        if(report_result[name].length>=k){ // 신고인이 k명 이상인 경우
+            report_result[name].map((user)=>{ // 해당 신고인의 이름을 가져와서
+                answer[user]+=1 // 메일 발송 횟수를 늘려준다
+            })
+        }
+    })
+    
     answer=Object.values(answer)
     return answer
 }
