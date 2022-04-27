dae5993a3bb05824e86ba43b26421ccbc36422f3
열일 <isawthat77@naver.com>
2022-04-27 17:33:47 +0900

    20220427

--- js for coding test/programmers_search_3.js
+++ js for coding test/programmers_search_3.js
@@ -1,52 +1,15 @@
 function solution(brown, yellow) {
-    var answer = []
-    let total_sum = brown+yellow
-    let divisor = []
-    let cnt =0;
+    var answer =[];
 
-    for(let i=0; i<=total_sum; i++){
-        if(total_sum%i===0){
-            divisor.push(i)
-            if(i*i === total_sum){
-                divisor.push(i)
-            }
+    for(var y=3; y<=yellow+2; y++){ //세로의 길이는 3부터 시작해서 yellow 면적보다 아래 위로 1씩 더 길어야 함
+        let x = (brown-2*y+4)/2 // brwon(모서리 개수) = 2x+2y-4
+        if(x>=y && x*y===brown+yellow){ // 가로 길이 >= 세로 길이, 가로길이*세로길이 = 총 면적인 경우
+            answer=[x,y]
         }
     }
-
-    const isTotalSum = (num) =>{
-        if(num[0] * num[1] === total_sum && (num[0]*2 + num[1]*2) - 4 === brown
-        &&num[0] >= num[1]){
-            return true
-        } else{
-            return false
-        }
-    }
-
-    
-
-    const permutation = (arr) => {
-        while (arr.length-1>cnt){
-            for(let i=0; i<arr.length-1; i++){
-                let mult_comb=[]
-                let fixed = arr[0]
-                mult_comb.push(fixed)
-                mult_comb.push(arr[i+1])
-                if(isTotalSum(mult_comb)){
-                    answer=mult_comb
-                }
-            }
-            let temp_arr = [...arr]
-            temp_arr.shift()
-            temp_arr.push(arr[0])
-            cnt++
-            permutation(temp_arr)
-        }
-        return answer
-    }
-    answer = permutation(divisor)
     return answer;
 }
 
-let brown = 8
-let yellow = 1
+let brown = 24
+let yellow = 24
 solution(brown, yellow)
