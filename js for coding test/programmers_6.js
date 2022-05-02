5c1d718517f573fc723487cc5cfb4ba06c87f1f6
열일 <isawthat77@naver.com>
2022-05-02 17:18:35 +0900

    20220502

--- js for coding test/programmers_6.js
+++ js for coding test/programmers_6.js
@@ -1,24 +1,12 @@
-function solution(board, moves) {
+function solution(numbers) {
     var answer = 0;
-    var stack = []
-
-    moves.map(move=>{
-        for(let i=0; i<board.length; i++){
-            if(board[i][move-1]!=0){
-                stack.push(board[i][move-1])
-                board[i][move-1]=0
-                break
-            }
+    for(i=0; i<10; i++){
+        if(!numbers.includes(i)){
+            answer+=i
         }
-        if(stack[stack.length-1]===stack[stack.length-2] && stack.length>0){
-            stack.pop()
-            stack.pop()
-            answer+=2
-        }
-    })
+    }
     return answer;
 }
 
-let board = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]] // 격자 모양
-let moves = [1,5,3,5,1,2,1,4] // 크레인이 이동한 위치
-solution(board, moves)
+let numbers = [1,2,3,4,6,7,8,0]	
+solution(numbers)
