5675936953f2b0c1cef6c4b8da18157c5d2b78d2
열일 <isawthat77@naver.com>
2022-05-03 22:04:10 +0900

    20220503

---
+++ js for coding test/programmers_37.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    n = String(n)
+    for (let i = 0; i < n.length; i++) {
+        answer += Number(n[i])
+    }
+    return answer;
+}
+
+let n = 123
+solution(n)
