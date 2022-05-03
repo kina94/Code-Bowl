b2084b8a518721d51f88fffb4b4fabeb5e7958d5
열일 <isawthat77@naver.com>
2022-05-03 17:23:26 +0900

    20220503

---
+++ js for coding test/programmers_18.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 0;
+    for(let x=1; x<=n; x++){
+        if(n%x===1){
+            answer=x
+            break
+        }
+    }
+    return answer;
+}
+
+let n = 12
+solution(n)
