43a268ff9fb66f9271e960a85bbd902f9b4e5e65
열일 <isawthat77@naver.com>
2022-05-07 00:43:19 +0900

    20220507

---
+++ js for coding test/programmers_53.js
@@ -0,0 +1,47 @@
+function solution(p) {
+    var answer = '';
+    var open = 0;
+    var close = 0;
+
+    const check = (p) => { //올바른 문자열인지 판단
+        let stack = []
+        for (let i = 0; i < p.length; i++) {
+            if (p[i] === '(') stack.push('(')
+            else {
+                if (stack.length === 0) return false
+                stack.pop()
+            }
+        }
+        return true
+    }
+
+    const makeBasket = (p) => {
+        if (!p) return ''; // 1번 (빈 문자열 반환)
+        for (let i = 0; i < p.length; i++) {
+            if (p[i] === '(') open++
+            else close++ // 괄호 짝의 갯수가 맞는지 판단
+
+            if (open === close) {// 균형일 때 올바른 괄호 문자열인지 판단 (2)
+                if (check(p.slice(0, i + 1))) { // slice한 u가 올바른 문자열인지 판단 (3)
+                    answer = p.slice(0, i + 1) + makeBasket(p.slice(i + 1)) //올바른 문자열이면 v에 대해 1단계부터 다시 수행하고 u에 이어붙임
+                    return answer
+                } else{// u가 올바른 문자열이 아니라면
+                    answer = '(' + makeBasket(p.slice(i+1)) + ')' //4-1, 4-2, 4-3 수행
+                    for (let j=1; j<i; j++){ //4-4. u의 앞뒤 제거
+                        if (p[j]==='(') answer+=')'
+                        else answer+='('
+                    }
+                    return answer
+                } 
+            }
+        }
+    }
+
+
+    answer = makeBasket(p)
+    return answer
+}
+
+
+let p = "()))((()"
+solution(p)
