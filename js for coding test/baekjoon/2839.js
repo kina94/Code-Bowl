261a8b27c675a7828d7359fd4c69324ffd7f1c1f
열일 <isawthat77@naver.com>
2022-05-19 21:48:35 +0900

    0519

---
+++ js for coding test/baekjoon/2839.js
@@ -0,0 +1,20 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let n = Number(input);
+let answer = 0;
+
+while(true){
+    if(n % 5 === 0){
+        answer = answer + (n / 5)
+        break
+    }
+    if(n < 3){
+        answer = -1
+        break
+    }
+    n = n - 3;
+    answer++
+}
+
+console.log(answer)
