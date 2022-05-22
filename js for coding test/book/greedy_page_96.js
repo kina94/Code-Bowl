f5f277ce2d5d8b99da0a53880070b6afa1974873
열일 <isawthat77@naver.com>
2022-05-23 01:38:01 +0900

    0523

---
+++ js for coding test/book/greedy_page_96.js
@@ -0,0 +1,14 @@
+function solution(n){
+    let min = 1000
+    let array = []
+    for(let i=0; i<n.length; i++){
+        let card = Math.min(...n[i])
+        array.push(card)
+    }
+    let answer = Math.max(...array)
+    return answer
+}
+
+let n = [[7,3,1,8],[3,3,3,4]]
+
+solution(n)
