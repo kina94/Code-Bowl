7961ceff4dfdad84541f12a8a17fedb99ac8aec7
열일 <isawthat77@naver.com>
2022-12-06 00:51:35 +0900

    스택큐_다리를 지나는 트럭

--- js for coding test/programmers/SQ_5.js
+++ js for coding test/programmers/SQ_5.js
@@ -1,30 +1,23 @@
 function solution(bridge_length, weight, truck_weights) {
   let time = 0;
-  let complete = [];
-  let during = [];
-  while (truck_weights.length > 0) {
-    console.log(
-      "경과시간",
-      time,
-      "다리를 지난 트럭",
-      complete,
-      "다리를 건너는 트럭",
-      during,
-      "대기 트럭",
-      truck_weights
-    );
-    const truck = truck_weights[0];
-    truck_weights.shift();
-    during.push(truck);
+  let sum = 0;
+  let bridge = new Array(bridge_length).fill(0);
+
+  while (bridge.length > 0) {
     time++;
-    if (during.reduce((a, b) => a + b) >= 10) {
-      complete.push(truck);
-      during.shift();
-      time++;
-    } else {
-      continue;
+    sum -= bridge.shift();
+    if (truck_weights.length > 0) {
+      if (sum + truck_weights[0] <= weight) {
+        let truck = truck_weights.shift();
+        sum += truck;
+        bridge.push(truck);
+      } else {
+        bridge.push(0);
+      }
     }
   }
+
+  return time;
 }
 
 solution(2, 10, [7, 4, 5, 6]);
